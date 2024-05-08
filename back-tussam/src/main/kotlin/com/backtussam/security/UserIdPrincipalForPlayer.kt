5442d312b118a047e684752c0d6e819ab65fbf54
Juliangar2503 <jgq325@gmail.com>
2024-05-08 17:24:04 +0200

    JWT cheeck

--- back-tussam/src/main/kotlin/com/backtussam/security/UserIdPrincipalForPlayer.kt
+++ back-tussam/src/main/kotlin/com/backtussam/security/UserIdPrincipalForPlayer.kt
@@ -2,4 +2,6 @@ package com.backtussam.security
 
 import io.ktor.server.auth.*
 
+// Principal para el jugador
+// Utiliza para representar la identidad de un jugador en el contexto de autenticación
 data class UserIdPrincipalForPlayer(val id:String): Principal
