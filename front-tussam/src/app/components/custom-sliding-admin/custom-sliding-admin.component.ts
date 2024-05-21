f7b46815608fc5b19b48cff7147b2cbceb68d0c4
Juliangar2503 <jgq325@gmail.com>
2024-05-21 23:14:51 +0200

    Admin-medio-decente

--- front-tussam/src/app/components/custom-sliding-admin/custom-sliding-admin.component.ts
+++ front-tussam/src/app/components/custom-sliding-admin/custom-sliding-admin.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Player } from 'src/app/interfaces/player';
 import { EditPlayerComponent } from '../edit-player/edit-player.component';
+import { BackTussamService } from 'src/app/services/back-tussam.service';
 
 @Component({
   selector: 'app-custom-sliding-admin',
@@ -11,11 +12,18 @@ import { EditPlayerComponent } from '../edit-player/edit-player.component';
 export class CustomSlidingAdminComponent {
 
   @Input() player!: Player;
-  constructor(private modalController: ModalController) { }
+  constructor(
+    private modalController: ModalController,
+    private backSvc: BackTussamService
+  ) { }
 
 
-  deleteCharacter() {
-    // Tu lógica para eliminar el personaje aquí
+  deletePlayer() {
+    this.backSvc.deletePlayer(this.player.id).subscribe(
+      res => {
+        console.log(res);
+      }
+    );
   }
 
   async openEditModal(player: Player) {
--- front-tussam/src/app/services/back-tussam.service.ts
+++ front-tussam/src/app/services/back-tussam.service.ts
@@ -16,9 +16,13 @@ export class BackTussamService {
 
   constructor(private http: HttpClient) { }
 
+  // ******  AUTH  ******
+
   login(email: string, password: string): Observable<ApiResponse> {
     return this.http.post<any>(environment.baseUrl + environment.login, { email, password });
   }
+
+  // ***************** PLAYERS *****************
 
   getRanking(idLeague: Number):Observable<ApiResponsePlayers>{
     return this.http.get<any>(environment.baseUrl + environment.Ranking + idLeague);
@@ -35,6 +39,10 @@ export class BackTussamService {
 
   editPlayer(email:String, player: EditPlayerParams | null):Observable<ApiResponse>{
     return this.http.put<any>(environment.baseUrl + environment.player + email, player);
+  }
+
+  deletePlayer(id:Number):Observable<ApiResponse>{
+    return this.http.delete<any>(environment.baseUrl + environment.player + id);
   }
 
   // ***************** LEAGUES *****************
