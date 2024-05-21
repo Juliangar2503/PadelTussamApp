6210530ffb1ab35128aabc35034d53a4223291c7
Juliangar2503 <jgq325@gmail.com>
2024-05-21 23:41:42 +0200

    solucionado el problema de las ligas con los usuarios

--- front-tussam/src/app/services/utils.service.ts
+++ front-tussam/src/app/services/utils.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { BackTussamService } from './back-tussam.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,11 +8,11 @@ import { Router } from '@angular/router';
 export class UtilsService {
 
   constructor(
-    private router: Router
+    private router: Router,
+    private backSvc: BackTussamService
   ) { }
  
   // ---------------- NAVEGACIÓN -------------------- //
-
   goToPage(pageRoute: string) {
     this.router.navigate([pageRoute]);
   }
@@ -32,7 +33,4 @@ export class UtilsService {
     const item = localStorage.getItem(key);
     return item ? JSON.parse(item) : null;
   }
-
-
-
 }
