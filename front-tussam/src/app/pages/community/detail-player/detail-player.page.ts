da5b28cad74575b38d861e2e68b588eb79789a2d
Juliangar2503 <jgq325@gmail.com>
2024-06-02 00:06:51 +0200

    al buscar jugador también puedes ver su historial

--- front-tussam/src/app/pages/community/detail-player/detail-player.page.ts
+++ front-tussam/src/app/pages/community/detail-player/detail-player.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ModalController } from '@ionic/angular';
+import { Match } from 'src/app/interfaces/match';
 import { Player } from 'src/app/interfaces/player';
+import { StatsPlayer } from 'src/app/interfaces/stats-player';
 import { BackTussamService } from 'src/app/services/back-tussam.service';
 import { UtilsService } from 'src/app/services/utils.service';
 
@@ -13,13 +15,15 @@ import { UtilsService } from 'src/app/services/utils.service';
 export class DetailPlayerPage implements OnInit {
 
   //Creamos la variable id que será donde almacemos el id que recogemos de la pagina
-  id:any;
+  id: any;
 
   idNumber: number = 0;
-  player: Player = {} as Player;
+  player: Player = {} as Player;  
+  matches: Match[] = [];
+  statsPlayer: StatsPlayer = {} as StatsPlayer;
 
   constructor(
-    private svcBack: BackTussamService,
+    private backSvc: BackTussamService,
     private utilSvc: UtilsService,
     //Clase necesaria para recuperar el id
     private activateRoute: ActivatedRoute
@@ -31,18 +35,63 @@ export class DetailPlayerPage implements OnInit {
     this.obtenerPlayer();
   }
 
-  obtenerPlayer(){
-    this.svcBack.getPlayer(this.id).subscribe((res) => {
+  
+
+   /******* OBTENER PARTIDOS ********/
+
+   segmentChanged(event: CustomEvent) {
+    switch (event.detail.value) {
+      case 'openMatches':
+        this.getOpenMatches();
+        break;
+      case 'closedMatches':
+        this.getCloseMatches();
+        break;
+    }
+  }
+
+   getCloseMatches() {
+    this.backSvc.getMatchesCloseByPlayer(this.player.id).subscribe((data) => {
+      console.log('Matches');
+      console.log(data);
+      this.matches = data.data;
+      console.log(this.matches);
+    });
+  }
+
+  getOpenMatches() {
+    this.backSvc.getMatchesOpenByPlayer(this.player.id).subscribe((data) => {
+      this.matches = data.data;
+      console.log(this.matches);
+    });
+  }
+
+
+
+  /******* OBTENER DATOS DEL JUGADOR ********/
+
+  obtenerPlayer() {
+    this.backSvc.getPlayer(this.id).subscribe((res) => {
       this.player = res.data;
       console.log(this.player);
     });
+    this.getPlayerHistoryStats(this.id)
   }
+
+
 
   getAvatarImage(player: Player) {
     if (player.avatar) {
       return 'data:image/jpeg;base64,' + player.avatar;
     }
     return null;
+  }
+
+  getPlayerHistoryStats(playerId: Number) {
+    this.backSvc.getHistoryPlayerStats(playerId).subscribe((res) => {
+      console.log(res);
+      this.statsPlayer = res.data;
+    });
   }
 
 }
